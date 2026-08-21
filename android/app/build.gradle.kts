d60ed345f392a102d56aca7af804970910182d89
djzeez4real007 <djzeez4real007@gmail.com>
2026-08-22 00:27:33 +0100

    Upgrade compileSdk and targetSdk to 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,9 @@ plugins {
 
 android {
     namespace = "com.example.okikiola_enterprise"
-    compileSdk = flutter.compileSdkVersion
+    
+    // Explicitly set compileSdk to 36 to satisfy plugin requirements
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -15,20 +17,17 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.okikiola_enterprise"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        
+        // Explicitly set targetSdk to 36 to match compileSdk
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
