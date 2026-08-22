6f2526e8ccf10844b4d18b3fb7a40d3ccd00dfc4
djzeez4real007 <djzeez4real007@gmail.com>
2026-08-22 02:05:43 +0100

    Fix Gradle afterEvaluate

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,19 +15,7 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
-subprojects {
-    project.evaluationDependsOn(":app")
-}
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
-}
-subprojects {
-    afterEvaluate {
-        if (plugins.hasPlugin("com.android.application") || plugins.hasPlugin("com.android.library")) {
-            extensions.configure<com.android.build.gradle.BaseExtension> {
-                compileSdkVersion(36)
-            }
-        }
-    }
 }
